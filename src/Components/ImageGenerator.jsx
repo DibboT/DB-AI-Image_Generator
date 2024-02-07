0ab722599399ba63a3a889dc4176de30772c89c3
Dibbo T <dibbotripura7@gmail.com>
2024-02-07 11:40:03 +0600

    db ai image generator

---
+++ src/Components/ImageGenerator.jsx
@@ -0,0 +1,15 @@
+import './ImageGenerator.css'
+
+const ImageGenerator = () => {
+    return (
+        <div className='ai-image-generator'>
+            <div className='header'>AI Image <span>Generator</span></div>
+            <div className='img-loading'>
+                <div className='image'><img src="https://staticg.sportskeeda.com/editor/2021/12/34aff-16391409747063-1920.jpg" alt="" /></div>
+
+            </div>
+        </div>
+    );
+};
+
+export default ImageGenerator;
